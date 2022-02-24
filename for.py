ec840693a720b28b1c4feffeea3a20f4629d252b
Rajeshwari <99334198+Rajeshwari-CuriousData@users.noreply.github.com>
2022-02-24 17:03:22 +0530

    Add files via upload
    This file consists of  for loop programs using range ( ) function and retreving elements in descending order and also odd numbers 
    using range indexing and slicing

---
+++ for.py
@@ -0,0 +1,28 @@
+'''
+str="Rajehwari"
+for ch in str:
+	print(ch)
+	          '''
+
+
+'''
+str="Rajehwari"
+n=len(str)
+for i in range (n):
+	print(str[i])
+	              '''
+
+
+# odd numbers
+'''
+for i in range(1,11,2):
+	print(i,end=',') '''
+
+for i in range(10,0,-1):
+
+		print(i,end=',')
+
+
+
+
+
